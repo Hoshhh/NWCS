18aadaba94cc5db94e61f5c8c11ebdbabd617f4c
Hoshhh <nyxious123@live.com>
2022-03-21 01:49:15 -0400

    Fixed the profession bar to link back to the main page on all professions

--- src/components/ProfessionsBar.js
+++ src/components/ProfessionsBar.js
@@ -4,13 +4,13 @@ const ProfessionsBar = () => {
     return (
         <div className="prof-bar">
             <div className="prof-container">
-                <a href="google.com" className="armoring">Armoring</a>
-                <a href="google.com" className="arcana">Arcana</a>
-                <a href="google.com" className="cooking">Cooking</a>
-                <a href="google.com" className="engineering">Engineering</a>
-                <a href="google.com" className="furnishing">Furnishing</a>
-                <a href="google.com" className="jewelcrafting">Jewelcrafting</a>
-                <a href="google.com" className="weaponsmithing">Weaponsmithing</a>
+                <a href="https://nwcs.netlify.app/" className="armoring">Armoring</a>
+                <a href="https://nwcs.netlify.app/" className="arcana">Arcana</a>
+                <a href="https://nwcs.netlify.app/" className="cooking">Cooking</a>
+                <a href="https://nwcs.netlify.app/" className="engineering">Engineering</a>
+                <a href="https://nwcs.netlify.app/" className="furnishing">Furnishing</a>
+                <a href="https://nwcs.netlify.app/" className="jewelcrafting">Jewelcrafting</a>
+                <a href="https://nwcs.netlify.app/" className="weaponsmithing">Weaponsmithing</a>
             </div>
         </div>
     )
